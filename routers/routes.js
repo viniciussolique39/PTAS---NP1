c63e6d43b0cebef042433de385621ec21645e547
gabriela <113440728+viniciussolique39@users.noreply.github.com>
2023-09-14 15:38:12 -0400

    Arrumando as rotas, de findUsers para searchUsers

--- routers/routes.js
+++ routers/routes.js
@@ -3,12 +3,12 @@ const routes = express.Router();
 
 const userController = require('../controllers/userController');
 
-/*
-routes.get('/users', userController.findUsers);
+
+routes.get('/users', userController.searchUsers);
 routes.post('/user', userController.createUser);
 routes.delete('/user/:id', userController.deleteUser);
 routes.put('/user/:id', userController.updateUser);
 routes.post('/user/authenticated', userController.authenticatedUser);
-*/
+
 
 module.exports = routes;
